49e2971f542f3b331ddb14dbb78bff53a7e455e9
jenzie68 <jacecabalo@gmail.com>
2025-02-07 23:02:09 +0400

    test: increment no. of item when clicking ATC btn

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,7 @@ import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
+import ProductPage from './components/ProductPage';
 
 describe('Product Page', () => {
   beforeEach(() => {
@@ -30,5 +31,18 @@ describe('Product Page', () => {
       await screen.findByText('Mens Casual Slim Fit').toBeInTheDocument();
 
       expect(global.fetch).toHaveBeenCalledTimes(1);
+    };
+
+  it('increment no.of cart when clicking ATC btn'),
+    async () => {
+      const user = userEvent.setup();
+
+      render(<App />);
+
+      const button = screen.getByRole('button', { name: 'Add To Cart' });
+
+      await user.click(button);
+
+      expect(screen.getByRole('itemCount').textContent).toMatch(/1/i);
     };
 });
