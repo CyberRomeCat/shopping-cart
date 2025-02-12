e33733cce25a6a0c3803768ab40b12688d1f61b6
jenzie68 <jacecabalo@gmail.com>
2025-02-12 20:23:31 +0400

    feat: make shop button link to shop

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
   return (
     <div className="main">
       <div className="content">
         <h1>Lorem Ipsum fjdkffs fdsfds hdjkas</h1>
-        <button>Menu</button>
+        <button>
+          <Link to="/shop">Shop</Link>
+        </button>
       </div>
     </div>
   );
